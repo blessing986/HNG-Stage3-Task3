f54c7a668269d2110e6d903516914c078520df63
Blessing U <blessing986@users.noreply.github.com>
2022-11-14 08:20:59 +0000

    chore: Added path to placeToStay page in the index file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css';
 import App from './App';
+import PlaceToStay from './pages/placeToStay';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+        <Route path='/placeToStay' element={<PlaceToStay />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
